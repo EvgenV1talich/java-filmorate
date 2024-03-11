04d04a9798a9e15631695a2f53efdb0ecb2c31fe
Evgeny <kharchenko.e.v@yandex.ru>
2024-03-11 22:20:54 +0300

    refactor: fix whitespace around in FilmNotFoundException

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class FilmNotFoundException extends RuntimeException{
+public class FilmNotFoundException extends RuntimeException {
     public FilmNotFoundException(String message) {
         super(message);
     }
