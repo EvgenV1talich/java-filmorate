ab2996cda5bbbd5196f7c17ecb8cad07a7e23088
Evgeny <kharchenko.e.v@yandex.ru>
2024-06-16 20:57:27 +0300

    fix: FriendRequest fixes

--- src/main/java/ru/yandex/practicum/filmorate/model/FriendRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FriendRequest.java
@@ -1,13 +1,41 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class FriendRequest {
 
     private Integer requestUser;
     private Integer responseUser;
     private LocalDateTime date;
+    private boolean firstApproved;
+    private boolean secondApproved;
     private String status;
 
+    public FriendRequest(Integer requestUser, Integer responseUser, LocalDateTime date, boolean firstApproved, boolean secondApproved, String status) {
+        this.requestUser = requestUser;
+        this.responseUser = responseUser;
+        this.date = date;
+        this.firstApproved = firstApproved;
+        this.secondApproved = secondApproved;
+        this.status = status;
+    }
 
+    public boolean checkStatus() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "FriendRequest{" +
+                "requestUser=" + requestUser +
+                ", responseUser=" + responseUser +
+                ", date=" + date +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
