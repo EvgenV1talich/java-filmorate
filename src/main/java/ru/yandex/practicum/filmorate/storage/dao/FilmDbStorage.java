2b18a2304afb02a9aaee206e1950bcbb8775ab7b
Evgeny <kharchenko.e.v@yandex.ru>
2024-06-22 00:06:07 +0300

    feat: connect to DB, add FilmDAO, UserDAO

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/FilmDbStorage.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.storage.dao;
+
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+
+import java.util.HashMap;
+
+public class FilmDbStorage implements FilmStorage {
+    @Override
+    public void createFilm(Film film) {
+
+    }
+
+    @Override
+    public void updateFilm(Film film) {
+
+    }
+
+    @Override
+    public void deleteFilm(Integer filmId) {
+
+    }
+
+    @Override
+    public Film getFilm(Integer filmId) {
+        return null;
+    }
+
+    @Override
+    public HashMap<Integer, Film> getFilms() {
+        return null;
+    }
+}
