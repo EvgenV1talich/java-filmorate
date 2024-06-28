04a5053edddfe84cc37ea68d81115eb6bc77edcf
Evgeny <kharchenko.e.v@yandex.ru>
2024-06-29 01:05:01 +0300

    feat: add new GenreController

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -1,38 +1,36 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.service.genre.GenreService;
+import ru.yandex.practicum.filmorate.dto.GenreDTO;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("/genres")
+@RequiredArgsConstructor
 public class GenreController {
 
     private final GenreService genreService;
 
-    @Autowired
-    public GenreController(GenreService genreService) {
-        this.genreService = genreService;
-    }
 
     @GetMapping
-    public List<Genre> getAll() {
-        log.info("Получен GET запрос по эндпоинту '/genres' на получение всех Genres");
-        return genreService.getAll();
+    public ResponseEntity<List<GenreDTO>> readAll() {
+        log.info("GET'/genres' all genres");
+        return new ResponseEntity<>(genreService.readAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Genre getById(@PathVariable Integer id) {
-        log.info("Получен GET запрос по эндпоинту '/genres/{}' на получение Genre по id", id);
-        return genreService.getById(id);
+    public ResponseEntity<GenreDTO> readById(@PathVariable Integer id) {
+        log.info("GET'/genres/{}' genre by id", id);
+        return new ResponseEntity<>(genreService.readById(id), HttpStatus.OK);
     }
 
 }
