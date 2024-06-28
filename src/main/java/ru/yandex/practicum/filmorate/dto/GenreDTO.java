6ef56c23c2daff01acb927904648fa005106d024
Evgeny <kharchenko.e.v@yandex.ru>
2024-06-29 01:14:32 +0300

    fix: add builder to GenreDTO

--- src/main/java/ru/yandex/practicum/filmorate/dto/GenreDTO.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/GenreDTO.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.filmorate.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.criteria.CriteriaBuilder;
 
 @Data
+@Builder
 public class GenreDTO {
 
     private Integer id;
