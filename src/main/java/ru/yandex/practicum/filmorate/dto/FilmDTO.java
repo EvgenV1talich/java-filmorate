c2e007b4f86ccfc9cd33fafcc2928901144a38f1
Evgeny <kharchenko.e.v@yandex.ru>
2024-06-29 01:13:17 +0300

    fix: fix FilmDTO

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmDTO.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmDTO.java
@@ -3,11 +3,10 @@ package ru.yandex.practicum.filmorate.dto;
 import lombok.Builder;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.MPA;
-
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Set;
 
 @Data
@@ -15,15 +14,18 @@ import java.util.Set;
 public class FilmDTO {
 
     private Integer id;
-    @NotNull
+
+    @NotBlank
     private String name;
-    @Size(min = 0, max = 200)
+
+    @Size(min = 0, max = 199)
     private String description;
-    @Past
+
     private LocalDate releaseDate;
+
+    @PositiveOrZero
     private Integer duration;
     private Set<Long> likes;
-    private ArrayList<Genre> genre;
+    private Set<Genre> genres;
     private MPA mpa;
-
 }
