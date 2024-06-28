96058d1565f368f967a205fdf2324d655a495404
Evgeny <kharchenko.e.v@yandex.ru>
2024-06-29 01:35:22 +0300

    feat: backup UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/dal/userdao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/userdao/UserDbStorage.java
@@ -1,59 +1,45 @@
 package ru.yandex.practicum.filmorate.dal.userdao;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DuplicateKeyException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
-import ru.yandex.practicum.filmorate.dal.mappers.UserMapper;
-import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
-import ru.yandex.practicum.filmorate.exceptions.UserValidationException;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.validators.UserValidator;
 
-import javax.persistence.EntityNotFoundException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Slf4j
-@Repository
-@RequiredArgsConstructor
-public class UserDbStorage implements UserStorage {
+@Component("userDBStorage")
+public class UserDBStorage implements UserStorage {
 
     private final JdbcTemplate jdbcTemplate;
-    private final UserMapper mapper;
+
+    @Autowired
+    public UserDBStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public User createUser(User user) {
-        /*String query = "INSERT INTO USERS\n" +
-                "(ID, EMAIL, LOGIN, NAME, BIRTHDAY)\n" +
-                "VALUES(?, ?, ?, ?, ?);";
-        jdbcTemplate.update(query, user.getId(),
-                user.getEmail(),
-                user.getLogin(),
-                user.getName(),
-                user.getBirthday());*/
         SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate).withTableName("users")
                 .usingGeneratedKeyColumns("id");
         Number key = simpleJdbcInsert.executeAndReturnKey(userToMap(user));
-        user.setId(key.longValue());
+        user.setId((Long) key);
         log.debug("User создан с ID {}.", user.getId());
         return user;
-        //return user;
     }
 
     @Override
     public User updateUser(User user) {
-
         if (user == null || user.getId() == null) {
-            throw new UserNotFoundException("User не найден");
+            throw new ValidationException("Невалидный User");
         }
         String sqlQuery = "UPDATE users SET email = ?, login = ?, name =?, birthday = ? WHERE id = ?";
         if (jdbcTemplate.update(sqlQuery, user.getEmail(), user.getLogin(), user.getName(), user.getBirthday(),
@@ -61,53 +47,56 @@ public class UserDbStorage implements UserStorage {
             log.debug(" User {} успешно обновлён", user.getId());
             return user;
         } else {
-            throw new UserNotFoundException("User с таким id не найден " + user.getId());
+            throw new EntityNotFoundException("User с таким id не найден");
         }
-
     }
 
     @Override
-    public void deleteUser(Long id) {
-        String query = "DELETE FROM USERS\n" +
-                "WHERE ID=?;";
-        jdbcTemplate.update(query, id);
+    public List<User> getUsers() {
+        String sqlQuery = "SELECT * FROM users ";
+        log.debug("Все User получены");
+        return jdbcTemplate.query(sqlQuery, this::mapToUser);
     }
 
     @Override
     public User getUser(Long id) {
-        String sqlQuery = String.format("SELECT * FROM users WHERE id = %d", id);
-        SqlRowSet userRows = jdbcTemplate.queryForRowSet(sqlQuery);
-        User user = new User();
-        if (userRows.first()) {
-            user.setId(userRows.getLong("id"));
-            user.setEmail(userRows.getString("email"));
-            user.setLogin(userRows.getString("login"));
-            user.setName(userRows.getString("name"));
-            user.setBirthday(userRows.getDate("birthday").toLocalDate());
-            user.setFriends(getAllFriendsByUser(user.getId()));
-        } else {
-            throw new EntityNotFoundException("Юзер с таким id не найден " + id);
+        String sqlQuery = "SELECT * FROM users WHERE id = ?";
+        try {
+            User user = jdbcTemplate.queryForObject(sqlQuery, this::mapToUser, id);
+            log.debug("User с ID {} получен.", id);
+            return user;
+        } catch (Throwable throwable) {
+            throw new EntityNotFoundException("User с таким id не найден");
         }
-        return user;
     }
 
-    /*public boolean contains(Long userId) {
-        String query = "EXISTS(SELECT id FROM \"users\" WHERE id = :userId);";
-        return (jdbcTemplate.update(query)) > 0;
-    }*/
+    private Set<Long> findFriendsByUserId(Long id) {
+        String sqlQuery = "SELECT friendid FROM friends WHERE userid = ?";
+        Set<Long> ids = new HashSet<>();
+        SqlRowSet friends = jdbcTemplate.queryForRowSet(sqlQuery, id);
+        while (friends.next()) {
+            ids.add(friends.getLong("friendid"));
+        }
+        log.debug("Получены все Friends (Users) у User c ID {}.", id);
+        return ids;
+    }
 
-    @Override
-    public List<User> getUsers() {
-        String query = "SELECT * FROM users";
-        return jdbcTemplate.query(query, mapper);
+    public User mapToUser(ResultSet rs, int rowNum) throws SQLException {
+        return User.builder()
+                .id(rs.getLong("id"))
+                .email(rs.getString("email"))
+                .login(rs.getString("login"))
+                .name(rs.getString("name"))
+                .birthday(rs.getDate("birthday").toLocalDate())
+                .friends(findFriendsByUserId(rs.getLong("id")))
+                .build();
     }
 
     @Override
     public void userAddFriend(Long userId, Long friendId) {
-        if (!UserValidator.validate(getUser(userId)) || !UserValidator.validate(getUser(friendId))) {
-            throw new UserValidationException("Невалидный юзер!");
-        }
-        String query = "INSERT INTO users_friends (request_user_id, response_user_id) VALUES (?,?)";
+        User user = getUser(userId);
+        User friend = getUser(friendId); // если юзер отсутствует - будет выброшено исключение
+        String query = "INSERT INTO friends (userid, friendid) VALUES (?,?)";
         try {
             jdbcTemplate.update(query, userId, friendId);
             log.debug("Friend с ID {} добавлен успешно в friend к User c ID {}.", friendId, userId);
@@ -115,16 +104,14 @@ public class UserDbStorage implements UserStorage {
             log.debug("Ошибка добавления Friend с ID {} в friend к User c ID {}.", friendId, userId);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
+
     }
 
     @Override
     public void userDeleteFriend(Long userId, Long friendId) {
-        if (UserValidator.validate(getUser(userId)) || UserValidator.validate(getUser(friendId))) {
-            throw new UserValidationException("Невалидный User!");
-        }
         getUser(userId); // для валидации
         getUser(friendId); // для валидации
-        String query = "DELETE FROM users_friends WHERE request_user_id = ? AND response_user_id = ?";
+        String query = "DELETE FROM friends WHERE userid = ? AND friendid = ?";
         if (jdbcTemplate.update(query, userId, friendId) != 0) {
             log.debug("У User с ID {} удалён friend (User) {}", userId, friendId);
         } else {
@@ -132,21 +119,22 @@ public class UserDbStorage implements UserStorage {
         }
 
     }
-
-    public Set<Long> getAllFriendsByUser(Long id) {
-        /*getUser(id);
+    public List<User> getAllFriendsUser(Long id) {
+        getUser(id); // для валидации
         String query = "SELECT * FROM users WHERE id IN (SELECT friendid FROM friends WHERE userid = ?)";
-        List<User> friends = jdbcTemplate.query(query, mapper);
+        List<User> friends = jdbcTemplate.query(query, this::mapToUser, id);
         log.debug("Получен список friendId друзей у User с id {}.", id);
-        return friends;*/
-        String sqlQuery = "SELECT response_user_id FROM users_friends WHERE request_user_id = ?";
-        Set<Long> ids = new HashSet<>();
-        SqlRowSet friends = jdbcTemplate.queryForRowSet(sqlQuery, id);
-        while (friends.next()) {
-            ids.add(friends.getLong("response_user_id"));
+        return friends;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        String query = "DELETE FROM users WHERE id = ?";
+        if (jdbcTemplate.update(query, id) != 0) {
+            log.info("User с Id {} удалён.", id);
+        } else {
+            log.info("User с Id {} не найден.", id);
         }
-        log.debug("Получены все Friends (Users) у User c ID {}.", id);
-        return ids;
     }
 
     public Map<String, Object> userToMap(User user) {
