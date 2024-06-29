7ea82dd7f768f90fa46d1e5abffa60e5f7d7c193
Evgeny <kharchenko.e.v@yandex.ru>
2024-06-30 00:47:20 +0300

    style fix

--- src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MPA.java
@@ -10,7 +10,7 @@ public class MPA {
     private Integer id;
     private String name;
 
-    public MPA(Integer id, String name, Integer films_id) {
+    public MPA(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
